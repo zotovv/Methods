a58956c0cde1c863ce793943e0e4082f6673aec0
Vladimir Zotov <zott14@outlook.com>
2018-04-02 09:48:37 +0300

    Add Converter

---
+++ Converter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class Program
+    {
+        static double Conv(double currency, double exchange)
+        {
+            return currency * exchange;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму для конвертации: ");
+            double currency = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите курс валюты для конвертации: ");
+            double exchange = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Перевод суммы денег {0} по курсу {1} в сумму {2}", currency, exchange, Conv(currency, exchange));
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
